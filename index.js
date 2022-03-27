c7bbc92f37adf6efa9f771fc3dcc5e5a2bc559e4
ol0nny99 <100658149+ol0nny99@users.noreply.github.com>
2022-03-27 13:20:45 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solutions in this file
+const newArr = [];
+function writeCards(guests, event) {
+    for (let i = 0; i < guests.length; i++) {
+        newArr.push(`Thank you, ${guests[i]}, for the wonderful ${event} gift!`);
+    }  return newArr;}
+function countDown() {
+    let countDown = 10;
+    while (countDown > -1) {
+        console.log(countDown--);
+        }}
